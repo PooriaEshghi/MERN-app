08fcdec710b153f6fb76c287f9a1c73c69ab6648
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-05-16 15:32:22 +0200

    fixed user and goal backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,51 +1,59 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-
-const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel");
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
+
   if (!name || !email || !password) {
-    res.status(400);
-    throw new Error("Please add all fields");
+    res.status(400)
+    throw new Error('Please add all fields')
   }
-  const userExist = User.findOne({ email });
-  if (userExist) {
-    res.status(400);
-    throw new Error("User already exists");
+
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
   }
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(password, salt);
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
   const user = await User.create({
     name,
     email,
-    password: hashPassword,
-  });
+    password: hashedPassword,
+  })
+
   if (user) {
     res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(400);
-    throw new Error("Invalid user data");
+    res.status(400)
+    throw new Error('Invalid user data')
   }
-});
+})
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   // Check for user email
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
 
   if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
@@ -53,28 +61,29 @@ const loginUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(400);
-    throw new Error("Invalid credentials");
+    res.status(400)
+    throw new Error('Invalid credentials')
   }
-});
+})
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "Get Me!" });
-});
+  res.status(200).json(req.user)
+})
 
 // Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.EXPIRES,
-  });
-};
+    expiresIn: '30d',
+  })
+}
+
 module.exports = {
   registerUser,
   loginUser,
   getMe,
-};
+}
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const goalsSchema = mongoose.Schema(
+const goalSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "User",
+      ref: 'User',
     },
     text: {
       type: String,
-      require: [true, "Please add a text value"],
+      required: [true, 'Please add a text value'],
     },
   },
   {
     timestamps: true,
   }
-);
+)
 
-module.exports = mongoose.model("Goal", goalsSchema);
+module.exports = mongoose.model('Goal', goalSchema)
